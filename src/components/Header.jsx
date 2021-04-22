9565e291501c59b35307a7892d41bceb85bb8d81
Gabriel Kardasz <gabrielkardasz86@gmail.com>
2021-04-22 14:28:49 -0300

    Basico-Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; // se debe ejecytar siempre cada vez que creas un component
+
+function Header(){
+
+    return(
+        <h1>desde el Header</h1>
+    )
+}
+
+export default Header;
